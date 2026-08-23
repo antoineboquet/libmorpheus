8693b8e4ad7539d7fb67cc12753c6df9262fb82a
Antoine Boquet <antoineboquet@users.noreply.github.com>
2026-08-23 19:57:42 +0200

    Add public API symbol manifest

---
+++ cmake/PublicApi.cmake
@@ -0,0 +1,23 @@
+# Functions exported by ABI version 1. This manifest is shared by the binary
+# symbol check and the documentation-coverage check.
+set(MORPHEUS_PUBLIC_API_SYMBOLS
+  morpheus_abi_version
+  morpheus_analysis_size
+  morpheus_analyze
+  morpheus_close
+  morpheus_compat_analyze
+  morpheus_compat_output_analysis_count
+  morpheus_compat_output_data
+  morpheus_compat_output_free
+  morpheus_compat_output_lemma_count
+  morpheus_compat_output_length
+  morpheus_open
+  morpheus_open_path
+  morpheus_result_all_morph_flags
+  morpheus_result_copy
+  morpheus_result_count
+  morpheus_result_free
+  morpheus_result_get
+  morpheus_result_truncated_fields
+  morpheus_status_message
+)
