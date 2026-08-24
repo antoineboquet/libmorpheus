a074bac4140a6573d0236052c9f6241894060d43
Antoine Boquet <antoineboquet@users.noreply.github.com>
2026-08-24 20:07:01 +0200

    Parse optional fixture arguments without matching errors

--- test/test-fixtures.cmake
+++ test/test-fixtures.cmake
@@ -18,22 +18,45 @@ math(EXPR fixture_last "${fixture_count} - 1")
 foreach(fixture_index RANGE 0 ${fixture_last})
   string(JSON input GET "${fixtures_json}" ${fixture_index} input)
   string(JSON expected GET "${fixtures_json}" ${fixture_index} expected)
-  string(
-    JSON option_count ERROR_VARIABLE options_error
-    LENGTH "${fixtures_json}" ${fixture_index} opts
-  )
+  string(JSON fixture_member_count LENGTH
+         "${fixtures_json}" ${fixture_index})
+  set(has_options FALSE)
+  if(fixture_member_count GREATER 0)
+    math(EXPR fixture_member_last "${fixture_member_count} - 1")
+    foreach(fixture_member_index RANGE 0 ${fixture_member_last})
+      string(JSON fixture_member MEMBER
+             "${fixtures_json}" ${fixture_index} ${fixture_member_index})
+      if(fixture_member STREQUAL "opts")
+        set(has_options TRUE)
+        break()
+      endif()
+    endforeach()
+  endif()
+
   set(options "")
-  if(options_error STREQUAL "NOTFOUND")
+  if(has_options)
+    string(JSON options_type TYPE
+           "${fixtures_json}" ${fixture_index} opts)
+    if(NOT options_type STREQUAL "ARRAY")
+      message(FATAL_ERROR
+        "fixture ${fixture_index} has a non-array opts member in ${MORPHEUS_FIXTURES}")
+    endif()
+    string(JSON option_count LENGTH
+           "${fixtures_json}" ${fixture_index} opts)
     if(option_count GREATER 0)
       math(EXPR option_last "${option_count} - 1")
       foreach(option_index RANGE 0 ${option_last})
-        string(JSON option GET "${fixtures_json}" ${fixture_index} opts ${option_index})
+        string(JSON option_type TYPE
+               "${fixtures_json}" ${fixture_index} opts ${option_index})
+        if(NOT option_type STREQUAL "STRING")
+          message(FATAL_ERROR
+            "fixture ${fixture_index} option ${option_index} is not a string")
+        endif()
+        string(JSON option GET
+               "${fixtures_json}" ${fixture_index} opts ${option_index})
         list(APPEND options "${option}")
       endforeach()
     endif()
-  elseif(NOT options_error MATCHES "not found$")
-    message(FATAL_ERROR
-      "fixture ${fixture_index} has an invalid opts member in ${MORPHEUS_FIXTURES}: ${options_error}")
   endif()
 
   set(input_file "${MORPHEUS_WORK_DIR}/fixture-${fixture_index}.input")
